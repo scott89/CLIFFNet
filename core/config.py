9fbdcd58498995557fcf3bd25deebc38763d04f0
sott89 <scott89@github.com>
2020-01-08 06:54:05 +0800

    train nyu_perc_v2.3, using scene_v1.0 models

--- core/config.py
+++ core/config.py
@@ -40,11 +40,11 @@ config.train.max_epoch = 500
 config.train.lr_decay_epoch = 8
 config.train.display_iter = 20
 config.train.summary_iter = 100
-config.train.output_path = 'models/nyu_perc_v2.2/'
+config.train.output_path = 'models/nyu_perc_v2.3/'
 
 config.train.resume = False
 config.train.snapshot = 'models/nyu_base_v1.0/epoch-0.pth'
-config.train.perc_snapshot = 'models/nyu_scene_v2.0/epoch-16.pth'
+config.train.perc_snapshot = 'models/nyu_scene_v1.0/epoch-18.pth'
 config.train.pretrained = False
 config.train.pretrained_backbone = 'models/resnet-50-caffe.pth'
 
@@ -56,6 +56,6 @@ config.test.augment.canonical_size = 228
 config.test.augment.max_size = [512]
 
 
-config.gpu_id = '1'
+config.gpu_id = '0'
 os.environ['CUDA_VISIBLE_DEVICES'] = config.gpu_id
 config.gpu = ['cuda:%s'%i for i,j in enumerate(config.gpu_id.split(','))]
