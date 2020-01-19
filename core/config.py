54b539e1b1f7ea68d3e270a20fe1cd1aa4402dec
sott89 <scott89@github.com>
2020-01-19 22:18:54 +0800

    train nyu_perc_v5.0 on old data with fixed random seed

--- core/config.py
+++ core/config.py
@@ -9,7 +9,7 @@ config = edict()
 config.dataset = edict()
 config.dataset.name = 'NYU'
 config.dataset.data_path = '/media/4TB/Research/DataSet/NYU2/'
-config.dataset.train_data_file = 'train_uniform_60k.p'
+config.dataset.train_data_file = 'train_old_40k.p'
 config.dataset.val_data_file = 'test.p'
 config.dataset.crop_size = [456, 608]
 
@@ -32,7 +32,7 @@ config.train.augment.rotation = -1
 
 config.train.batch_size = 8
 config.train.lr = 1e-4
-config.train.lr_decay_iterations = [45000, 90000, 135000] #, 120000, 160000]
+config.train.lr_decay_iterations = [80000, 120000, 160000] #, 120000, 160000]
 config.train.warmup_it = -1
 config.train.perc_loss_warmup = 100
 config.train.momentum = 0.9
@@ -41,7 +41,7 @@ config.train.max_epoch = 500
 config.train.lr_decay_epoch = 8
 config.train.display_iter = 20
 config.train.summary_iter = 100
-config.train.output_path = 'models/nyu_perc_v4.1_r1_10_r2_15/'
+config.train.output_path = 'models/nyu_perc_v5.0/'
 
 config.train.resume = False
 config.train.snapshot = 'models/nyu_perc_v4.0/epoch-3.pth'
@@ -57,6 +57,6 @@ config.test.augment.canonical_size = 228
 config.test.augment.max_size = [512]
 
 
-config.gpu_id = '3'
+config.gpu_id = '0'
 os.environ['CUDA_VISIBLE_DEVICES'] = config.gpu_id
 config.gpu = ['cuda:%s'%i for i,j in enumerate(config.gpu_id.split(','))]
